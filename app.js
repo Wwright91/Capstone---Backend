26a4e01c8a792658f3403183302bad96dbc03b2e
Wwright91 <wisdomwright-martin@pursuit.org>
2023-10-20 03:23:22 -0400

    Fetch made to google places api via axios 1. Installed axios 2. Created function to fetch data from google places using the place_id from the req.query

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const cors = require("cors");
 const express = require("express");
+const axios = require("axios")
 
 const app = express();
 
@@ -8,6 +11,17 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome");
+});
+
+const API_key = process.env.REACT_APP_GOOGLE_API_KEY;
+
+app.get('/places/details', async (req, res) => {
+  try {
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?&place_id=${req.query.place_id}&key=${API_key}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data from Google Places API' });
+  }
 });
 
 const businessesController = require("./controllers/businessesController.js");
