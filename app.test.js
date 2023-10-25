5bc0a0b2c1854cd1025f41beb4cf3cfdbef4261c
Wwright91 <wisdomwright-martin@pursuit.org>
2023-10-25 11:06:34 -0400

    Created test for place_id 1. This test ensures that the call to the API returns a JSON object for the correct business that matches the given place_id.

--- app.test.js
+++ app.test.js
@@ -5,3 +5,10 @@ test("It should respond with Welcome", async () => {
   const res = await request(app).get("/");
   expect(res.text).toBe("Welcome");
 });
+
+test("It should return json object of business that matches the place_id", async () => {
+    const place_id = "ChIJb8Xw0wz2wokRXhp1-HSLvkI"
+    const res = await request(app).get(`/places/details?place_id=${place_id}`)
+    const data = JSON.parse(res.text).result;
+    expect(data.place_id).toBe(`${place_id}`)
+})
